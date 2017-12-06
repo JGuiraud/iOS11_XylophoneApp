a71aa17ce201b874ed5e3ea012734d2584b31957
Jérôme Guiraud <jerome31guiraud@gmail.com>
2017-12-06 21:17:19 +0100

    sound1 played when any key pressed

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,29 +1,43 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
+
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+//
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
         
+        audioPlayer.play()
         
-        
+//        switch sender.tag {
+//        case 1:
+//            print("note 1")
+//        case 2:
+//            print("note 2")
+//        case 3:
+//            print("note 3")
+//        case 4:
+//            print("note 4")
+//        case 5:
+//            print("note 5")
+//        case 6:
+//            print("note 6")
+//        default:
+//            print("note 7")
+//        }
     }
-    
-  
 
 }
 
